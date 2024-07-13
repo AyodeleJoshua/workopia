3b1eeaf615811e06b94207e3442d33927c7c29c8
JoshuaAyodele <jayodele@getreliancehealth.com>
2024-07-13 15:49:31 +0100

    create auth routes, views and controller

--- App/views/partials/message.php
+++ App/views/partials/message.php
@@ -3,11 +3,11 @@
         <?= $_SESSION['success_message'] ?>
     </div>
     <?php unset($_SESSION['success_message']); ?>
-<? endif; ?>
+<?php endif; ?>
 
 <?php if (isset($_SESSION['error_message'])): ?>
     <div class="message bg-red-100 p-3 my-3">
         <?= $_SESSION['errorr_message'] ?>
     </div>
     <?php unset($_SESSION['error_message']); ?>
-<? endif; ?>
+<?php endif; ?>
